0aa4271501bdfc536c2796b577a1e4ca7c07c277
Colin Kiama <colinkiama@hotmail.co.uk>
2018-11-03 21:30:19 +0000

    This client can now get access tokens!

--- FreedomUWP/Core/OAuth.cs
+++ FreedomUWP/Core/OAuth.cs
@@ -1,10 +1,12 @@
 ﻿using FreedomUWP.View;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -14,6 +16,9 @@ namespace FreedomUWP.Core
     {
         public async static Task Authenticate()
         {
+
+            const string _authCodeString = "code";
+
             // Contact developers@medium.com to get your client ID and client secret.
             var oAuthClient = new Medium.OAuthClient(Constants._clientID, Constants._clientSecret);
 
@@ -24,20 +29,42 @@ namespace FreedomUWP.Core
                 new[]
                 {
                     Medium.Authentication.Scope.BasicProfile,
-                    Medium.Authentication.Scope.PublishPost
+                    Medium.Authentication.Scope.ListPublications,
+                    Medium.Authentication.Scope.PublishPost,
                 });
 
             url = url.Remove(url.LastIndexOf('&'));
+            
+            
             // (Send the user to the authorization URL to obtain an authorization code.)
-            string authCode = await GetAuthCode(url);
+            string result = await GetAuthCode(url);
 
-            Debug.WriteLine(authCode);
+            // Attempt to parse authorisation code from result
+            // (Since result may not be a URL)
+            try
+            {
+                
+                Uri resultUri = new Uri(result);
+                NameValueCollection queryDictionary = HttpUtility.ParseQueryString(resultUri.Query);
+                string authCode = queryDictionary[_authCodeString];
+                Debug.WriteLine(authCode);
 
-            // Exchange the authorization code for an access token.
-            //var accessToken = oAuthClient.GetAccessToken("YOUR_AUTHORIZATION_CODE", Constants.callbackURL);
+                //Exchange the authorization code for an access token.
 
-            //// When your access token expires, use the refresh token to get a new one.
-            //var newAccessToken = oAuthClient.GetAccessToken(accessToken.RefreshToken);
+
+                var accessToken = oAuthClient.GetAccessToken(authCode, Constants.callbackURL);
+
+                // When your access token expires, use the refresh token to get a new one.
+                var newAccessToken = oAuthClient.GetAccessToken(accessToken.RefreshToken);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+
         }
 
         private async static Task<string> GetAuthCode(string startURL)
@@ -50,12 +77,44 @@ namespace FreedomUWP.Core
 
             string result;
 
-            AuthView authView = new AuthView();
-            Frame currentFrame = (Frame)Window.Current.Content;
-            Shell thisShell = (Shell)currentFrame.Content;
-            thisShell.AddAuthView(authView);
-            result = authView.Authenticate(startURL, endURL);
-            
+
+            try
+            {
+                var webAuthenticationResult =
+                    await Windows.Security.Authentication.Web.WebAuthenticationBroker.AuthenticateAsync(
+                    Windows.Security.Authentication.Web.WebAuthenticationOptions.None,
+                    startURI,
+                    endURI);
+
+                switch (webAuthenticationResult.ResponseStatus)
+                {
+                    case Windows.Security.Authentication.Web.WebAuthenticationStatus.Success:
+                        // Successful authentication. 
+                        result = webAuthenticationResult.ResponseData.ToString();
+                        break;
+                    case Windows.Security.Authentication.Web.WebAuthenticationStatus.ErrorHttp:
+                        // HTTP error. 
+                        result = webAuthenticationResult.ResponseErrorDetail.ToString();
+                        break;
+                    default:
+                        // Other error.
+                        result = webAuthenticationResult.ResponseData.ToString();
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Authentication failed. Handle parameter, SSL/TLS, and Network Unavailable errors here. 
+                result = ex.Message;
+            }
+
+
+            //AuthView authView = new AuthView();
+            //Frame currentFrame = (Frame)Window.Current.Content;
+            //Shell thisShell = (Shell)currentFrame.Content;
+            //thisShell.AddAuthView(authView);
+            //result = authView.Authenticate(startURL, endURL);
+
             return result;
         }
     }
