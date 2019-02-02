5968a8f6f2ba624ab302612612adcd90b7d9dc43
Colin Kiama <colinkiama@gmail.com>
2019-02-02 12:10:02 +0000

    Fixes #7 Local settings constants are now visible in project.

---
+++ FreedomUWP/Core/AppConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreedomUWP.Core
+{
+    public static class AppConstants
+    {
+        public const string refreshDateKey = "refreshDate";
+        public const string accessTokenKey = "accessToken";
+        public const string refreshTokenKey = "refreshToken";
+        public const string TokenTypeKey = "tokenType";
+        public const string ScopeKey = "scope";
+
+    }
+}
